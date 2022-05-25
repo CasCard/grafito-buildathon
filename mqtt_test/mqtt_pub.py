7e4b6f9187f95408b0fd2975f6683bdf0c141dd5
lokeshtarala <lokeshtarala@gmail.com>
2022-05-25 21:31:38 +0530

    randomised

--- mqtt_test/mqtt_pub.py
+++ mqtt_test/mqtt_pub.py
@@ -2,6 +2,7 @@
 from http import client
 import paho.mqtt.client as mqtt
 import time
+import random
 
 client = mqtt.Client("test_client_pub")
 client.connect("localhost")
@@ -9,6 +10,6 @@ client.connect("localhost")
 # send temperature data for every 0.1 seconds
 prev_time = time.time()
 while True:
-    if (time.time() - prev_time) > 0.1:
-        client.publish("test_topic", 25)
+    if (time.time() - prev_time) > 1:
+        client.publish("test_topic", random.randint(25, 35))
         prev_time = time.time()
