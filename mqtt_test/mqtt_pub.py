9db2238e28dbd330820bd5e4516dc1acbd3b8d9e
CasCard <abelcheruvathoor@gmail.com>
2022-05-26 07:03:22 +0530

    added function for publish_metrics

--- mqtt_test/mqtt_pub.py
+++ mqtt_test/mqtt_pub.py
@@ -7,18 +7,21 @@ import random
 client = mqtt.Client("test_client_pub")
 client.connect("192.168.199.142")
 
+def publish_metrics(Location):
+    # publish a message
+    client.publish(f"temperature/{Location}", random.randint(0,100))
+    client.publish(f"humidity/{Location}", random.randint(0,100))
+    client.publish(f"pressure/{Location}", random.randint(0,100))
+    client.publish(f"moisture/{Location}", random.randint(0,100))
+
 # send temperature data for every 0.1 seconds
 prev_time = time.time()
 while True:
     if (time.time() - prev_time) > 1:
         # client.publish("test_topic", random.randint(25, 35))
-        client.publish("temperature/location_one", 22)
-        client.publish("temperature/location_two", 23)
-
-        client.publish("humidity/location_one", random.randint(25, 35))
-        client.publish("humidity/location_two", random.randint(25, 35))
-
-        client.publish("humidity/location_one", random.randint(25, 35))
-        client.publish("humidity/location_two", random.randint(25, 35))
+        publish_metrics("Kalamassery")
+        publish_metrics("Cheranallur")
+        publish_metrics("Kottayam")
+        publish_metrics("Aluva")
         
         prev_time = time.time()
